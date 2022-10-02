ba19722241e78d973e6b255b84d1cdb39d5d63d2
Jake Deakin <jake.deakin@outlook.com>
2022-10-02 15:09:56 +1000

    Player Shooting Added
    Added in Line projectile for player to shoot
    Added ammo
    Added how often player can shoot (shots per fire, and shots per second)
    Can change between automatic and single shot per trigger press
    Ammo counter added to the Pistol
    Added enlarged collider to Enemy prefab so aim doesn't have to be so perfect
    
    TextMeshPro added to project which is most of the files added in this push.
    
    TODO: Add gun sound effects, muzzle flash, proper gun model, damage to enemies

--- Assets/Scripts/WeaponScripts/WeaponController.cs
+++ Assets/Scripts/WeaponScripts/WeaponController.cs
@@ -1,26 +1,45 @@
+using Newtonsoft.Json.Bson;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+using TMPro;
 
 public class WeaponController : MonoBehaviour
 {
-    public float bulletCount;
+    public float currentBulletCount;
+    public float maxBulletCount;
     public float bulletSpeed;
     public float bulletDamage;
     public float firingSpeed;
-    public float shotsPerFire;
+    public int shotsPerFire;
+    public float shotsLeft;
+    public float weaponRange;
+
+    public AudioClip gunFire;
+    public AudioClip reload;
+    public AudioClip noAmmo;
+
     public GameObject bulletSpawn;
     public GameObject projectile;
 
     public PlayerInputHandler playerInputHandler;
     public ActionBasedController controller;
+
+    public float shotTime;
+    TextMeshProUGUI ammoText;
+
     RaycastHit hit;
+
     // Start is called before the first frame update
     void Start()
     {
         playerInputHandler = GetComponentInParent<PlayerInputHandler>();
-        controller = GetComponentInParent<ActionBasedController>(); 
+        controller = GetComponentInParent<ActionBasedController>();
+        shotsLeft = shotsPerFire;
+        shotTime = Time.time;
+        ammoText = GetComponentInChildren<TextMeshProUGUI>();
+        ammoText.text = currentBulletCount.ToString();
     }
 
     // Update is called once per frame
@@ -30,31 +49,112 @@ public class WeaponController : MonoBehaviour
         {
             if (controller.name.Contains("Left"))
             {
-                if (playerInputHandler.GetLeftFireInputDown())
+                if (playerInputHandler.GetLeftFireInputHeld())
                 {
-                    Fire();
-                    print("bang");
+                    if (Time.time > shotTime + 1 / firingSpeed)
+                    {
+                        TryFire();
+
+                        print("left trigger held");
+                    }
+                }
+
+                if (playerInputHandler.GetLeftFireInputReleased())
+                {
+                    shotsLeft = shotsPerFire;
+                    shotTime = 0;
                 }
             }
 
             if (controller.name.Contains("Right"))
             {
-                if (playerInputHandler.GetRightFireInputDown())
+                
+                if (playerInputHandler.GetRightFireInputHeld())
                 {
-                    Fire();
-                    print("bang");
+                    if (Time.time > shotTime + 1 / firingSpeed)
+                    {
+                        TryFire();
+
+                        print("right trigger held");
+                    }
+
+                }
+
+                if (playerInputHandler.GetRightFireInputReleased())
+                {
+                    shotsLeft = shotsPerFire;
+                    shotTime = 0;
                 }
             }
-            
+
+
+            if ((Vector3.Angle(bulletSpawn.transform.up, Vector3.up) > 80 || Vector3.Angle(bulletSpawn.transform.up, Vector3.up) < -80) && currentBulletCount != maxBulletCount)
+            {
+                Reload();
+            }
+
         }
     }
 
     public void Fire()
     {
-        if (Physics.Raycast(bulletSpawn.transform.position, transform.up * 100, out hit))
+        currentBulletCount--;
+        shotsLeft--;
+        shotTime = Time.time;
+        ammoText.text = currentBulletCount.ToString();
+
+        if (Physics.Raycast(bulletSpawn.transform.position, transform.up * weaponRange, out hit))
         {
             print(hit.collider.name);
             Debug.DrawLine(bulletSpawn.transform.position, hit.point, Color.yellow, 100);
+            GameObject line = Instantiate(projectile);
+            line.GetComponent<LineRenderer>().SetPositions(new Vector3[] { bulletSpawn.transform.position, hit.point });
+            Destroy(line, 0.2f);
         }
+    }
+
+    public bool CanFire()
+    {
+        if (HasAmmo() && HasShotsLeft())
+        {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public bool HasAmmo()
+    {
+        if (currentBulletCount > 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool HasShotsLeft()
+    {
+        if (shotsLeft > 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public void TryFire()
+    {
+        if (CanFire())
+        {
+            Fire();
+        }
+    }
+
+    public void Reload()
+    {
+            currentBulletCount = maxBulletCount;
+            ammoText.text = currentBulletCount.ToString();
+            print("Reload");
     }
 }
