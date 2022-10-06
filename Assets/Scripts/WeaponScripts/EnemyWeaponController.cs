118e7d005458beb1df198b0504a810988def5345
Jake Deakin <jake.deakin@outlook.com>
2022-10-07 00:22:57 +1000

    Tidy up
    Enemies Prefab to group all enemies together in a scene
    New folder for utility scripts
    New namespace for math from WeaponController script
    (needs a better name)
    Removed math from Weaponcontroller and used reference to math namespace instead
    Duplicated WeaponController and made one for player and enemy instead

--- Assets/Scripts/WeaponScripts/EnemyWeaponController.cs
+++ Assets/Scripts/WeaponScripts/EnemyWeaponController.cs
@@ -8,7 +8,7 @@ using Unity.Mathematics;
 using UnityEngine.InputSystem.XR;
 using ShootyBorbVRMath;
 
-public class WeaponController : MonoBehaviour
+public class EnemyWeaponController : MonoBehaviour
 {
     public float currentBulletCount;
     public float maxBulletCount;
@@ -52,6 +52,7 @@ public class WeaponController : MonoBehaviour
         shotTime = Time.time;
         ammoText = GetComponentInChildren<TextMeshProUGUI>();
         ammoText.text = currentBulletCount.ToString();
+
 
         if (playerInputHandler == null)
         {
@@ -188,46 +189,4 @@ public class WeaponController : MonoBehaviour
         else
             instance.velocity = (targetCamera.transform.position - transform.position).normalized * bulletSpeed;
     }
-
-    //// Math courtesy of jean-gobert de coster on Youtube. Works beautifully! https://www.youtube.com/user/jeangodecoster
-
-    //public bool InterceptionDirection(Vector3 a, Vector3 b, Vector3 vA, float sB, out Vector3 result)
-    //{
-    //    var aToB = b - a;
-    //    var dC = aToB.magnitude;
-    //    var alpha = Vector3.Angle(aToB, vA) * Mathf.Deg2Rad;
-    //    var sA = vA.magnitude;
-    //    var r = sA / sB;
-
-    //    if (SolveQuadratic(1-r*r, 2*r*dC*Mathf.Cos(alpha), -(dC*dC), out var root1, out var root2) == 0)
-    //    {
-    //        result = Vector3.zero;
-    //        return false;
-    //    }
-
-    //    var dA = Mathf.Max(root1, root2);
-    //    var t = dA / sB;
-    //    var c = a + vA * t;
-    //    result = (c - b).normalized;
-    //    return true;    
-
-    //}
-
-    //// Math courtesy of jean-gobert de coster on Youtube. Works beautifully! https://www.youtube.com/user/jeangodecoster
-    //public static int SolveQuadratic(float a, float b, float c, out float root1, out float root2)
-    //{
-    //    var discriminant = b * b - 4 * a * c;
-    //    if (discriminant < 0)
-    //    {
-    //        root1 = Mathf.Infinity;
-    //        root2 = -root1;
-    //        return 0;
-    //    }
-
-    //    root1 = (-b + Mathf.Sqrt(discriminant)) / (2 * a);
-    //    root2 = (-b - Mathf.Sqrt(discriminant)) / (2 * a);
-    //    return discriminant > 0 ? 2 : 1;
-    //}
-
-
 }
